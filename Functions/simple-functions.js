0932182b8a793733ab6776ed972e0d622c195ef8
Chauni <chaunidbrown@gmail.com>
2023-05-01 10:25:58 -0400

    added functions folder and exercise file

---
+++ Functions/simple-functions.js
@@ -0,0 +1,19 @@
+function favoriteThings() {
+    console.log('My name is Chauni');
+    console.log('My favorite movie is Lilo & Stitch');
+    console.log('My favorite band is Paramore');
+}
+
+favoriteThings();
+
+function whyImHere() {
+    console.log('I joined this program to give myself a chance to pursue a career that I enjoy and have a lot of interest in.');
+}
+
+whyImHere();
+
+function favoritePlace() {
+    console.log('My favorite place to visit is Los Angeles because that is where most of my family lives, and also because the weather is almost always nice over there.');
+}
+
+favoritePlace();
